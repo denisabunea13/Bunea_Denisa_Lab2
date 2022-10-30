0fbcd098faf34c91a9b493133306c3fc964bdcac
Admin <Admin@ASUSZENBOOK>
2022-10-30 20:11:19 +0200

    Add project files.

---
+++ Data/Bunea_Denisa_Lab2Context.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Bunea_Denisa_Lab2.Models;
+
+namespace Bunea_Denisa_Lab2.Data
+{
+    public class Bunea_Denisa_Lab2Context : DbContext
+    {
+        public Bunea_Denisa_Lab2Context (DbContextOptions<Bunea_Denisa_Lab2Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Bunea_Denisa_Lab2.Models.Book> Book { get; set; } = default!;
+
+        public DbSet<Bunea_Denisa_Lab2.Models.Author> Author { get; set; }
+
+        public DbSet<Bunea_Denisa_Lab2.Models.Publisher> Publisher { get; set; }
+    }
+}
---
+++ Pages/Books/Index.cshtml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using Bunea_Denisa_Lab2.Data;
+using Bunea_Denisa_Lab2.Models;
+
+namespace Bunea_Denisa_Lab2.Pages.Books
+{
+    public class IndexModel : PageModel
+    {
+        private readonly Bunea_Denisa_Lab2.Data.Bunea_Denisa_Lab2Context _context;
+
+        public IndexModel(Bunea_Denisa_Lab2.Data.Bunea_Denisa_Lab2Context context)
+        {
+            _context = context;
+        }
+
+        public IList<Book> Book { get;set; } = default!;
+
+        public async Task OnGetAsync()
+        {
+            if (_context.Book != null)
+            {
+                Book = await _context.Book
+                    .Include(b => b.Publisher)
+                    .Include(b => b.Author)
+                    .ToListAsync();
+            }
+        }
+    }
+}
