0a31d769c0f5d336e9b7d8ef9d037c98120fcfba
Denisa Bunea <denibunea1311@gmail.com>
2022-11-01 17:03:27 +0200

    actualizare

--- Pages/Books/Edit.cshtml.cs
+++ Pages/Books/Edit.cshtml.cs
@@ -19,35 +19,40 @@ namespace Bunea_Denisa_Lab2.Pages.Books
             _context = context;
         }
         [BindProperty]
-        public Book Book { get; set; }
+        public Book Book { get; set; } = default!;
+
         public async Task<IActionResult> OnGetAsync(int? id)
         {
-            if (id == null)
+            if (id == null || _context.Book == null)
             {
                 return NotFound();
             }
             Book = await _context.Book
-            .Include(b => b.Publisher)
-            .Include(b => b.BookCategories).ThenInclude(b => b.Category)
-            .AsNoTracking()
-            .FirstOrDefaultAsync(m => m.ID == id);
+              .Include(b => b.Author)
+              .Include(b => b.Publisher)
+              .Include(b => b.BookCategories).ThenInclude(b => b.Category)
+              .AsNoTracking()
+              .FirstOrDefaultAsync(m => m.ID == id);
+            //var book =  await _context.Book.FirstOrDefaultAsync(m => m.ID == id);
             if (Book == null)
             {
                 return NotFound();
             }
-            //apelam PopulateAssignedCategoryData pentru o obtine informatiile necesare checkbox-
-            //urilor folosind clasa AssignedCategoryData
             PopulateAssignedCategoryData(_context, Book);
             var authorList = _context.Author.Select(x => new
             {
                 x.ID,
                 FullName = x.LastName + " " + x.FirstName
             });
+
             ViewData["AuthorID"] = new SelectList(authorList, "ID", "FullName");
-            ViewData["PublisherID"] = new SelectList(_context.Publisher, "ID",
-           "PublisherName");
+            ViewData["PublisherID"] = new SelectList(_context.Set<Publisher>(), "ID",
+"PublisherName");
             return Page();
         }
+
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see https://aka.ms/RazorPagesCRUD.
         public async Task<IActionResult> OnPostAsync(int? id, string[]
 selectedCategories)
         {
@@ -56,6 +61,7 @@ selectedCategories)
                 return NotFound();
             }
             var bookToUpdate = await _context.Book
+            .Include(_b => _b.Author)
             .Include(i => i.Publisher)
             .Include(i => i.BookCategories)
             .ThenInclude(i => i.Category)
@@ -67,8 +73,8 @@ selectedCategories)
             if (await TryUpdateModelAsync<Book>(
             bookToUpdate,
             "Book",
-            i => i.Title, i => i.Author,
-            i => i.Price, i => i.PublishingDate, i => i.Publisher))
+            i => i.Title, i => i.AuthorID,
+            i => i.Price, i => i.PublishingDate, i => i.PublisherID))
             {
                 UpdateBookCategories(_context, selectedCategories, bookToUpdate);
                 await _context.SaveChangesAsync();
