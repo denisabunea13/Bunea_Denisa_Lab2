8ca034c5613e83eebafab14c4ecc283f6b253b71
Denisa Bunea <denibunea1311@gmail.com>
2022-11-08 17:06:42 +0200

    lab4

---
+++ Models/ViewModels/PublisherIndexData.cs
@@ -0,0 +1,10 @@
+﻿using Bunea_Denisa_Lab2.Models;
+namespace Bunea_Denisa_Lab2.Models.ViewModels
+
+{
+    public class PublisherIndexData
+    {
+        public IEnumerable<Publisher> Publishers { get; set; }
+        public IEnumerable<Book> Books { get; set; }
+    }
+}
